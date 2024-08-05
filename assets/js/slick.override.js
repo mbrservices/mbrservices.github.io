552440391b307487ef0efcd4649970bf51430d93
Marcel Brückner <marcelbrueckner@users.noreply.github.com>
2024-08-05 23:46:07 +0200

    Add slider navigation on desktop

---
+++ assets/js/slick.override.js
@@ -0,0 +1,6 @@
+// Override Portio theme's default service slider configuration
+$(document).ready(function () {
+    $(".service__slider").slick('slickSetOption', {
+        dots: true
+     }, true);
+});
